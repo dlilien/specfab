874d4dcc279eba99f0fd62d4cd8fc6201c3a69ad
David Lilien <dlilien90@gmail.com>
2021-07-07 17:55:56 +0200

    Does an absolute path fix it?

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+import os
 from numpy.distutils.core import Extension
 from numpy.distutils.core import setup
 
+pwd = os.path.abspath(os.path.curdir)
 
 extension_module = Extension(name='specfab.specfabpy',
                              sources=['specfab/src/specfabpy.f90'],
-                             library_dirs=['specfab/src'],
+                             library_dirs=[os.path.join(pwd, 'specfab/src')],
                              libraries=['specfab'],
                              extra_f90_compile_args=['-ffree-line-length-none', '-mcmodel=medium'])
 
