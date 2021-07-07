4e1060fc7b2d5f371fb39378578f1af951ea318e
David Lilien <dlilien90@gmail.com>
2021-07-07 13:58:27 +0200

    deps on mac, gfortran on linux

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@
 yum install -y lapack-devel fftw-devel
 export LD_LIBRARY_PATH=`$PWD`/specfab/src:$LD_LIBRARY_PATH 
 export LD_LIBRARY_PATH=/opt/rh/devtoolset-9/root/lib/gcc/x86_64-redhat-linux/9:$LD_LIBRARY_PATH 
+export FC=/opt/rh/devtoolset-9/root/usr/bin/gfortran
 
 # Build specfab
 make lib
